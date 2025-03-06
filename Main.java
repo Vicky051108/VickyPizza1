10703af7c29db128d7ce8656ee9716d157b93ac2
Vicky051108 <3299632485@qq.com>
2025-03-06 17:56:41 +0800

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,10 +1,13 @@
-public class Main{
+
+
+public class Main {
     public static void main(String[] args) {
-        
-        VickyPizza vickypizza = new vickypizza("Slice O Heaven", " Heaven Street", "heaven@email.com", "1234567890", "cheesePizza", "tomatoSause, cheese, dough", 89.00, "fries", "coke", 8, 97.00 );
-        System.out.println("Taking Order");
-        info.takeOrder(vickypizza.getStoreMenu(), vickypizza.getPizzaIngredients(), vickypizza.getPizzaPrice(), vickypizza.getSides(), vickypizza.getDrinks());
-        System.out.println("Making Pizza");
-        info.makePizza(vickypizza.getPizzaIngredients());
+        VickyPizza order = new VickyPizza();
+        order.takeOrder();
+        order.isItYourBirthday();
+        order.makeCardPayment();
+        order.processCardPayment();
+        System.out.println(order);
     }
+
 }
